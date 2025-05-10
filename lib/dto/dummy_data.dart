9fc8059fe2d3de3f15cb238524bc9c82e97f21be
Pimol <pimoloem@gmail.com>
2025-05-10 14:11:54 +0700

    feat: data testing

---
+++ lib/dto/dummy_data.dart
@@ -0,0 +1,8 @@
+import '../models/participant.dart';
+
+List<Participant> dummyParticipants = [
+  Participant(id: '#4', name: 'da'),
+  Participant(id: '#2', name: 'ka'),
+  Participant(id: '#3', name: 'lu'),
+  Participant(id: '#1', name: 'ta'),
+];
