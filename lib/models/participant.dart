56f790e804f7fc47794f77d7cad75009bff592ba
rainn22 <nonsorany859@gmail.com>
2025-05-10 18:00:21 +0700

    refactor: change participant model add id

--- lib/models/participant.dart
+++ lib/models/participant.dart
@@ -1,6 +1,17 @@
 class Participant {
   final String id;
   final String name;
+  final int bib;
 
-  Participant({required this.id, required this.name});
+  Participant({required this.id, required this.name, required this.bib});
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Participant &&
+          runtimeType == other.runtimeType &&
+          id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
