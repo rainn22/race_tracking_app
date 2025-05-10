5a94d5e9f5327dfaa12588e7bd92a23ecc84ff13
rainn22 <nonsorany859@gmail.com>
2025-05-10 18:02:42 +0700

    feat: add landing screen for race control

---
+++ lib/utils/widgets/race_control_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RaceControlView extends StatelessWidget {
+  const RaceControlView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Race Control content here"));
+  }
+}
